9506c8d2c5464b84b9ef20668fe0bfba37d638b2
Zoltan Puha <zoltan.puha@zalando.de>
2024-12-10 09:51:50 +0100

    move reader

--- aoc24.py
+++ aoc24.py
@@ -1,9 +1,26 @@
 import polars as pl
 
-def polars_printer(df: pl.DataFrame):
-    """"
+
+def polars_printer(df: pl.DataFrame) -> None:
+    """ "
     Helper function to print non-truncated numbers
     """
     with pl.Config(set_fmt_float="full"):
         print(df)
     return None
+
+
+def aoc_reader(file_path: str, number_of_cols: int = 2) -> pl.DataFrame:
+    """
+    Reads in a csv to be compatible with day 1 format
+    """
+    df = pl.read_csv(file_path, has_header=False)
+    df = (
+        df.with_columns(
+            pl.col("column_1").str.split_exact("   ", n=number_of_cols).alias("col1")
+        )
+        .unnest("col1")
+        .drop("column_1")
+        .cast(pl.Float32)
+    )
+    return df
--- day1/day1.py
+++ day1/day1.py
@@ -1,40 +1,38 @@
 import polars as pl
-from aoc24 import polars_printer
+from aoc24 import polars_printer, aoc_reader
 
 
 def part1():
-    df = pl.read_csv("day1/day1.csv", has_header=False)
-    df = df.with_columns(
-        pl.col("column_1").str.split_exact("   ", n=2).alias("col1")
-    ).unnest("col1")
+    df = aoc_reader("day1/day1.csv")
     df = df.select("field_0", "field_1").with_columns(
         r1=pl.col("field_0").rank("ordinal"),
         r2=pl.col("field_1").rank("ordinal").cast(pl.Float32),
     )
-    polars_printer(pl.concat(
-                    [
-                        df.sort("r1").select("field_0").cast(pl.Float32),
-                        df.sort("r2").select("field_1").cast(pl.Float32),
-                    ],
-                    how="horizontal",
-                )
-                .with_columns(diff=abs((pl.col("field_1") - pl.col("field_0"))))
-                .select(pl.sum("diff"))
-            )
+    polars_printer(
+        pl.concat(
+            [
+                df.sort("r1").select("field_0"),
+                df.sort("r2").select("field_1"),
+            ],
+            how="horizontal",
+        )
+        .with_columns(diff=abs((pl.col("field_1") - pl.col("field_0"))))
+        .select(pl.sum("diff"))
+    )
 
 
 part1()
 
 
 def part2():
-    df = pl.read_csv("day1/day1.csv", has_header=False)
-    df = df.with_columns(
-        pl.col("column_1").str.split_exact("   ", n=2).alias("col1")
-    ).unnest("col1")
+    df = aoc_reader("day1/day1.csv")
     df_count = df.group_by("field_1").len()
-    polars_printer(df.select(pl.col("field_0"))
-            .join(df_count, left_on="field_0", right_on="field_1")
-            .with_columns(sim=pl.col("field_0").cast(pl.Float32) * pl.col("len"))
-            .select(pl.sum("sim")))
+    polars_printer(
+        df.select(pl.col("field_0"))
+        .join(df_count, left_on="field_0", right_on="field_1")
+        .with_columns(sim=pl.col("field_0").cast(pl.Float32) * pl.col("len"))
+        .select(pl.sum("sim"))
+    )
+
 
 part2()
